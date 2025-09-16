f2601af9b4862d0a7c23457024626055b6c43fb6
Bhabananda Das <blendersbd@gmail.com>
2025-09-16 10:10:38 +0530

    Audio error

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -334,6 +334,7 @@ class Call(PyTgCalls):
               MediaStream(
                 media_path=link,
                 audio_parameters=audio_stream_quality,
+                video_flags=MediaStream.Flags.IGNORE,
               ),
               #stream_type=StreamType().pulse_stream,
           )
@@ -425,6 +426,7 @@ class Call(PyTgCalls):
                 MediaStream(
                   media_path=audio_url,
                   audio_parameters=audio_stream_quality,
+                  video_flags=MediaStream.Flags.IGNORE,
                 ),
                 #stream_type=StreamType().pulse_stream,
             )
